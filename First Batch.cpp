535f9a9d495a60f921571b0fc3ba2ced56a275c4
Igor Liberato <igcbsb@gmail.com>
2021-03-02 14:40:34 -0300

    Create First Batch.cpp
    Exercises 1.1 and 1.2
    
    Started and finished on 02/02/2021

---
+++ First Batch.cpp
@@ -0,0 +1,33 @@
+//Exercise 1.1
+
+#include <iostream>
+
+int main()
+{
+	char symbol;
+	std::cout << "symbol ?" << std::endl;
+	std::cin >> symbol;
+	std::cout << (int)symbol << std::endl;
+	return 0;
+}
+
+
+//Exercise 1.2
+
+#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	float mean, a_mean, b_mean, c_mean;
+	std::cout << "Please, input 3 integer numbers" << std::endl;
+	std::cin >> a >> b >> c;
+	mean = (((float)a) + b + c) / 3;
+	a_mean = a - mean;
+	b_mean = b - mean;
+	c_mean = c - mean;
+	std::cout << mean << std::endl << a_mean << std::endl <<
+		b_mean << std::endl << c_mean << std::endl;
+
+	return 0;
+}
